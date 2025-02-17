958cf8a0ba714a05b36e2e30d72e1483dbc7b730
Juandoqg <juandoqg@gmail.com>
2025-02-17 17:10:22 -0500

    prueba de modificacion

--- app/src/main/scala/taller/App.scala
+++ app/src/main/scala/taller/App.scala
@@ -6,7 +6,9 @@ package taller
 object App {
   def main(args: Array[String]): Unit = {
     println(greeting())
+    println(greeting2())
   }
 
-  def greeting(): String = "Hello, world!"
+  def greeting(): String = "Hello world!"
+  def greeting2(): String = "hola, prueba"
 }
